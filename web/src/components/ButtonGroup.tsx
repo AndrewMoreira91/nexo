1c561bb678bbfc31de85a76989974aa01b33350c
Andrew Moreira <andrewsantos9113@gmail.com>
2025-04-13 10:49:59 -0300

    Refactor ButtonGroup and ButtonPill components
    - Updated ButtonGroup component to accept an array of keys and values, and added support for disabling deselection.
    - Modified ButtonPill component to include an isDisabled prop for disabling the button.
    
    Refactor Progressbar component
    
    - Changed the background color of the Progressbar component to use the primary color.
    
    Refactor PomodoroPage component
    
    - Updated the selectedMode state to use 'focus' instead of 'pomodoro'.
    - Added conditional rendering for different modes in the PomodoroPage component.
    - Modified the handleClickReset function to call the changeTimeLeft function.
    - Updated the ButtonGroup component in the PomodoroPage component to use the new keys and values props, and added support for disabling deselection.
    
    Update styles.css
    
    - Added new CSS variables for primary, shortBreak, and longBreak colors.

--- web/src/components/ButtonGroup.tsx
+++ web/src/components/ButtonGroup.tsx
@@ -2,25 +2,28 @@ import { type FC, useState } from 'react'
 import ButtonPill from './ButtonPill'
 
 type ButtonGroupProps = {
-	values?: string[]
-	setSelectedValue?: (value: string) => void
+	values: string[]
+	keys: string[]
+	disableDeselection?: boolean
+	onValueSelect?: (value: string) => void
 }
 
-const ButtonGroup: FC<ButtonGroupProps> = ({ values, setSelectedValue }) => {
+const ButtonGroup: FC<ButtonGroupProps> = ({ keys, values, onValueSelect, disableDeselection = false }) => {
 	const [selected, setLocalSelected] = useState(0)
 	return (
 		<div className="flex flex-row gap-4">
-			{values?.map((value, index) => (
+			{keys?.map((value, index) => (
 				<ButtonPill
 					// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
 					key={index}
 					onClick={() => {
 						setLocalSelected(index)
-						setSelectedValue?.(value)
+						onValueSelect?.(value)
 					}}
 					theme={selected === index ? 'primary' : 'secondary'}
+					isDisabled={disableDeselection && selected !== index}
 				>
-					{value}
+					{values[index]}
 				</ButtonPill>
 			))}
 		</div>
