6d535d71e1ee3936cccaaa17feb456075566b488
Andrew Moreira <88143161+AndrewMoreira91@users.noreply.github.com>
2025-05-27 10:45:38 -0300

    Update api.ts

--- web/src/libs/api.ts
+++ web/src/libs/api.ts
@@ -24,6 +24,7 @@ api.interceptors.response.use(
 		if (error.code === "ECONNABORTED" && error.message.includes("timeout")) {
 			console.error("API request timed out:", error);
 			window.dispatchEvent(new CustomEvent("apiTimeout", { detail: error }));
+			return undefined;
 		}
 		if (error.response.data.error === "TokenExpiredError") {
 			localStorage.removeItem("accessToken");
