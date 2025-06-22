0d8aff627a945fbe704ac98ed0b39332b6a84f55
Andrew Moreira <andrewsantos9113@gmail.com>
2025-06-22 17:03:40 -0300

    feat: Enhance Header component with dropdown functionality and improve user experience

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Avatar, Dropdown } from "@mui/joy";
-import { useRef } from "react";
+import { Avatar } from "@mui/joy";
+import { useState } from "react";
 import { IoMdExit, IoMdSettings } from "react-icons/io";
 import { useNavigate } from "react-router";
 import LogoNexoIcon from "../assets/LogoNexoIcon";
@@ -9,8 +9,7 @@ const Header = () => {
   const navigate = useNavigate();
   const { isAuthenticated, user, logout } = useAuth();
 
-  const avatarRef = useRef<HTMLButtonElement>(null);
-  const dropdownRef = useRef<HTMLDivElement>(null);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   function handleClickOnLogo() {
     if (isAuthenticated) {
@@ -49,63 +48,68 @@ const Header = () => {
             </button>
             <button
               type="button"
-              onClick={() => dropdownRef.current?.classList.toggle("hidden")}
-              ref={avatarRef}
               className="cursor-pointer"
+              onMouseEnter={() => setDropdownOpen(true)}
+              onMouseLeave={() => setDropdownOpen(false)}
             >
-              <Dropdown>
-                <Avatar src="" size="lg" color="primary" alt={user?.name} />
-              </Dropdown>
+              <Avatar src="" size="lg" color="primary" alt={user?.name} />
             </button>
           </div>
         )}
-      </header>
-      {isAuthenticated && (
-        <div
-          ref={dropdownRef}
-          className="hidden flex-col gap-6 bg-white shadow-2xl p-6 rounded-2xl absolute left-0 sm:left-auto right-0 mx-6 sm:mx-16 top-[60px]"
-        >
-          <div className="flex flex-col">
-            <div className="flex flex-row gap-3">
-              <Avatar
-                src=""
-                size="md"
-                color="primary"
-                variant="outlined"
-                alt={user?.name}
-              />
-              <h3 className="font-semibold text-2xl">{user?.name}</h3>
+        {isAuthenticated && (
+          <div
+            className={`
+            ${
+              dropdownOpen ? "flex" : "hidden"
+            } flex-col gap-6 bg-white shadow-2xl p-6 rounded-2xl absolute left-0 sm:left-auto right-0 mx-6 sm:mx-16 top-[60px]
+            `}
+            onMouseEnter={() => setDropdownOpen(true)}
+            onMouseLeave={() => setDropdownOpen(false)}
+          >
+            <div className="flex flex-col">
+              <div className="flex flex-row gap-3">
+                <Avatar
+                  src=""
+                  size="md"
+                  color="primary"
+                  variant="outlined"
+                  alt={user?.name}
+                />
+                <h3 className="font-semibold text-2xl">{user?.name}</h3>
+              </div>
+              <div className="w-full h-[1px] bg-gray-300 mt-2" />
             </div>
-            <div className="w-full h-[1px] bg-gray-300 mt-2" />
-          </div>
 
-          <ul className="flex flex-col gap-6">
-            <button
-              type="button"
-              className="cursor-pointer hover:bg-gray-100 px-4 py-2 rounded-2xl"
-            >
-              <li className="flex flex-row gap-3 items-center">
-                <div className="bg-primary-bg p-2 rounded-full">
-                  <IoMdSettings size={25} className="text-primary" />
-                </div>
-                <span className="font-medium text-xl">Perfil e definições</span>
-              </li>
-            </button>
-            <button
-              type="button"
-              className="cursor-pointer hover:bg-gray-100 px-4 py-2 rounded-2xl"
-              onClick={logout}
-            >
-              <li className="flex flex-row gap-3 items-center">
-                <div className="bg-primary-bg p-2 rounded-full">
-                  <IoMdExit size={25} className="text-primary" />
-                </div>
-                <span className="font-medium text-xl">Sair da conta</span>
-              </li>
-            </button>
-          </ul>
-        </div>
-      )}
+            <ul className="flex flex-col gap-6">
+              <button
+                type="button"
+                className="cursor-pointer hover:bg-gray-100 px-4 py-2 rounded-2xl"
+              >
+                <li className="flex flex-row gap-3 items-center">
+                  <div className="bg-primary-bg p-2 rounded-full">
+                    <IoMdSettings size={25} className="text-primary" />
+                  </div>
+                  <span className="font-medium text-xl">
+                    Perfil e definições
+                  </span>
+                </li>
+              </button>
+              <button
+                type="button"
+                className="cursor-pointer hover:bg-gray-100 px-4 py-2 rounded-2xl"
+                onClick={logout}
+              >
+                <li className="flex flex-row gap-3 items-center">
+                  <div className="bg-primary-bg p-2 rounded-full">
+                    <IoMdExit size={25} className="text-primary" />
+                  </div>
+                  <span className="font-medium text-xl">Sair da conta</span>
+                </li>
+              </button>
+            </ul>
+          </div>
+        )}
+      </header>
     </div>
   );
 };
